799a77f1e7a6c8935792d2a05090541cb844bc00
Alex Bates <ard.bates@gmail.com>
2019-01-04 13:47:08 +0000

    Add further environment tests

--- test/env.scm
+++ test/env.scm
@@ -23,4 +23,11 @@
   (test-eqv "Look up value in two-tier env" 4 (lookup-variable-value 'c env))
   (test-eqv "Look up outer scope value in two-tier env" 2 (lookup-variable-value 'b env)))
 
+(let ((env (extend-environment '() '() the-empty-environment)))
+  (define-variable! 'a 1 env)
+  (test-eqv "Look up defined variable" 1 (lookup-variable-value 'a env))
+  (set-variable-value! 'a 2 env)
+  (test-eqv "Look up set variable" 2 (lookup-variable-value 'a env))
+  (test-error "Set undefined variable" (set-variable-value! 'b 3 env)))
+
 (test-end "env-test")
