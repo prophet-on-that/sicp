ba21a357107ac330b9508fbd684384cfacfef34f
Alex Bates <ard.bates@gmail.com>
2019-02-06 15:05:43 +0000

    Remove old eval work

--- repl.scm
+++ repl.scm
@@ -9,7 +9,7 @@
 (define (driver-loop)
   (prompt-for-input input-prompt)
   (let ((input (read)))
-    (let ((output (eval input the-global-environment)))
+    (let ((output (eval-analyse input the-global-environment)))
       (announce-output output-prompt)
       (user-print output)))
   (driver-loop))
