1c47522a47647d93592aa3607ee8f550dc6bd76f
Alex Bates <ard.bates@gmail.com>
2019-03-23 13:59:42 +0000

    Add register machine test suite

--- controllers.scm
+++ controllers.scm
@@ -1,6 +1,7 @@
 (define-module (sicp controllers))
 
-(use-modules (sicp reg-machine))
+(use-modules (sicp reg-machine)
+             (srfi srfi-64))
 
 ;;; GCD
 
@@ -44,3 +45,39 @@
      (assign val (const 1))
      (goto (reg continue))
      fact-done)))
+
+;;; Test suite
+
+(test-begin "reg-machine-test")
+
+(set-register-contents! gcd-machine 'a 30)
+(set-register-contents! gcd-machine 'b 45)
+(start gcd-machine)
+(test-eqv
+    "GCD machine operates correctly"
+  (get-register-contents gcd-machine 'a)
+  15)
+
+(set-register-contents! factorial-machine 'n 5)
+(start factorial-machine)
+(test-eqv
+    "Factorial machine operates correctly"
+  (get-register-contents factorial-machine 'val)
+  120)
+
+(test-error
+ "Throw error on duplicate labels"
+ (make-machine
+  '(a)
+  '()
+  '(start
+    (goto (label here))
+    here
+    (assign a (const 3))
+    (goto (label there))
+    here
+    (assign a (const 4))
+    (goto (label there))
+    there)))
+
+(test-end "reg-machine-test")
