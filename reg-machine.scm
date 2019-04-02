318dfd853999db2abda277d1862655d11e24c7e5
Alex Bates <ard.bates@gmail.com>
2019-04-02 14:11:44 +0100

    Add statistics to stack implementation

--- reg-machine.scm
+++ reg-machine.scm
@@ -31,22 +31,37 @@
 ;;; Stack
 
 (define (make-stack)
-  (let ((s '()))
+  (let ((s '())
+        (number-pushes 0)
+        (max-depth 0)
+        (current-depth 0))
     (define (push x)
-      (set! s (cons x s)))
+      (set! s (cons x s))
+      (set! number-pushes (+ 1 number-pushes))
+      (set! current-depth (+ 1 current-depth))
+      (set! max-depth (max current-depth max-depth)))
     (define (pop)
       (if (null? s)
           (error "Empty stack -- POP")
           (let ((top (car s)))
             (set! s (cdr s))
+            (set! current-depth (- current-depth 1))
             top)))
     (define (initialise)
       (set! s '())
+      (set! number-pushes 0)
+      (set! max-depth 0)
+      (set! current-depth 0)
       'done)
+    (define (print-statistics)
+      (newline)
+      (display (list 'total-pushes '= number-pushes
+                     'maximum-depth '= max-depth)))
     (define (dispatch message)
       (cond ((eq? message 'push) push)
             ((eq? message 'pop) (pop))
             ((eq? message 'initialise) (initialise))
+            ((eq? message 'print-statistics) (print-statistics))
             (else (error "Unknown request -- STACK" message))))
     dispatch))
 
@@ -164,7 +179,9 @@
         (stats (make-machine-stats)))
     (let ((the-ops
            (list (list 'initialise-stack
-                       (lambda () (stack 'initialise)))))
+                       (lambda () (stack 'initialise)))
+                 (list 'print-stack-statistics
+                       (lambda () (stack 'print-statistics)))))
           (register-table
            (list (list 'pc pc)
                  (list 'flag flag))))
