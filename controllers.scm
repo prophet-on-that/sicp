8cb62c11992627a55abfdb70690ffb4ddf0bc252
Alex Bates <ard.bates@gmail.com>
2019-04-02 14:20:03 +0100

    Add factorial read machine (ex 5.14)

--- controllers.scm
+++ controllers.scm
@@ -44,6 +44,34 @@
      (goto (reg continue))
      fact-done)))
 
+(define factorial-read-machine
+  (make-machine
+   (list (list '- -)
+         (list '= =)
+         (list '* *))
+   '((assign n (op read))
+     (perform (op initialise-stack))
+     (assign continue (label fact-done))
+     fact-loop
+     (test (op =) (reg n) (const 1))
+     (branch (label base-case))
+     (save continue)
+     (save n)
+     (assign n (op -) (reg n) (const 1))
+     (assign continue (label after-fact))
+     (goto (label fact-loop))
+     after-fact
+     (restore n)
+     (restore continue)
+     (assign val (op *) (reg n) (reg val))
+     (goto (reg continue))
+     base-case
+     (assign val (const 1))
+     (goto (reg continue))
+     fact-done
+     (perform (op print) (reg val))
+     (perform (op print-stack-statistics)))))
+
 ;;; Test suite
 
 (test-begin "reg-machine-test")
