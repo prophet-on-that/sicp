d0e59ab5ab8fa79c19e5246c2e8ac116facd037e
Alex Bates <ard.bates@gmail.com>
2019-05-16 13:11:45 +0100

    Use separate RET register for returning values
    This simplifies the code somewhat.

--- asm-interpreter.scm
+++ asm-interpreter.scm
@@ -12,10 +12,11 @@
 (define number-tag #x20000000)
 
 ;;; Register aliases
-(define rax 0)                          ; Used for return value
-(define rbx 1)
-(define rcx 2)
-(define rdx 3)
+(define ret 0)                          ; Used for return value
+(define rax 1)
+(define rbx 2)
+(define rcx 3)
+(define rdx 4)
 (define continue 4)
 
 ;;; Memory layout
@@ -29,7 +30,8 @@
 ;;; 2 - attempting to take CDR of a non-pair
 
 (define (init max-num-pairs)
-  `((alias ,rax rax)
+  `((alias ,ret ret)
+    (alias ,rax rax)
     (alias ,rbx rbx)
     (alias ,rcx rcx)
     (alias ,rdx rdx)
@@ -53,6 +55,7 @@
     ;; Returns: newly-assigned pair
     ;; TODO: trigger garbage collection when out of space
     cons
+    (stack-push (reg rax))
     (stack-push (reg rbx))
     (stack-push (reg rcx))
     (mem-load (reg rax) (const ,free-pair-pointer))
@@ -66,17 +69,18 @@
     (mem-store (reg rbx) (reg rcx))
     (assign (reg rbx) (op +) (reg rax) (const 1)) ; new free pair pointer
     (mem-store (const ,free-pair-pointer) (reg rbx))
-    (assign (reg rax) (op logior) (reg rax) (const ,pair-tag))
+    (assign (reg ret) (op logior) (reg rax) (const ,pair-tag))
     (stack-pop (reg rcx))
     (stack-pop (reg rbx))
+    (stack-pop (reg rax))
     (ret)
 
     ;; Args:
     ;; 0 - Object to test
     pair?
-    (mem-load (reg rax) (op +) (reg bp) (const 2)) ; arg 0
-    (assign (reg rax) (op logand) (reg rax) (const ,tag-mask))
-    (test (op =) (reg rax) (const ,pair-tag))
+    (mem-load (reg ret) (op +) (reg bp) (const 2)) ; arg 0
+    (assign (reg ret) (op logand) (reg ret) (const ,tag-mask))
+    (test (op =) (reg ret) (const ,pair-tag))
     (ret)
 
     ;; Args:
@@ -85,17 +89,19 @@
     ;; TODO: test for pair in Scheme CAR implementation, for
     ;; efficiency
     car
+    (stack-push (reg rax))
+    (stack-push (reg rbx))
     (mem-load (reg rax) (op +) (reg bp) (const 2)) ; Arg 0
     (stack-push (reg rax))
     (call pair?)
     (jez (label car-invalid-arg))
     (stack-pop (reg rax))
     (assign (reg rax) (op logand) (reg rax) (const ,value-mask)) ; Offset into pair arrays
-    (stack-push (reg rbx))
     (mem-load (reg rbx) (const ,the-cars-pointer))
     (assign (reg rax) (op +) (reg rax) (reg rbx))
-    (mem-load (reg rax) (reg rax))
+    (mem-load (reg ret) (reg rax))
     (stack-pop (reg rbx))
+    (stack-pop (reg rax))
     (ret)
     car-invalid-arg
     (error (const 1))
@@ -106,17 +112,19 @@
     ;; TODO: test for pair in Scheme CDR implementation, for
     ;; efficiency
     cdr
+    (stack-push (reg rax))
+    (stack-push (reg rbx))
     (mem-load (reg rax) (op +) (reg bp) (const 2)) ; Arg 0
     (stack-push (reg rax))
     (call pair?)
     (jez (label cdr-invalid-arg))
     (stack-pop (reg rax))
     (assign (reg rax) (op logand) (reg rax) (const ,value-mask)) ; Offset into pair arrays
-    (stack-push (reg rbx))
     (mem-load (reg rbx) (const ,the-cdrs-pointer))
     (assign (reg rax) (op +) (reg rax) (reg rbx))
-    (mem-load (reg rax) (reg rax))
+    (mem-load (reg ret) (reg rax))
     (stack-pop (reg rbx))
+    (stack-pop (reg rax))
     (ret)
     cdr-invalid-arg
     (error (const 2))
@@ -176,11 +184,12 @@
            (stack-push (reg rax))
            (assign (reg rax) (op logior) (const ,number-tag) (const 1))
            (stack-push (reg rax))
-           (call cons))))
-       (rax (get-machine-register machine rax))
+           (call cons)
+           (assign (reg sp) (op +) (reg sp) (const 2)))))
+       (ret (get-machine-register machine ret))
        (memory (get-machine-memory machine)))
   (start-machine machine)
-  (test-eqv (get-register-contents rax) (logior pair-tag 0))
+  (test-eqv (get-register-contents ret) (logior pair-tag 0))
   (test-eqv (get-memory memory free-pair-pointer) 1))
 
 ;;; Test pair?: true
@@ -191,8 +200,10 @@
            (assign (reg rax) (op logior) (const ,number-tag) (const 2))
            (stack-push (reg rax))
            (call cons)
-           (stack-push (reg rax))
-           (call pair?))))
+           (assign (reg sp) (op +) (reg sp) (const 2))
+           (stack-push (reg ret))
+           (call pair?)
+           (stack-pop))))
        (flag (get-machine-flag machine)))
   (start-machine machine)
   (test-eqv (get-register-contents flag) 1))
@@ -202,7 +213,8 @@
         (make-test-machine
          `((assign (reg rax) (op logior) (const ,number-tag) (const 1))
            (stack-push (reg rax))
-           (call pair?))))
+           (call pair?)
+           (stack-pop))))
        (flag (get-machine-flag machine)))
   (start-machine machine)
   (test-eqv (get-register-contents flag) 0))
@@ -215,18 +227,21 @@
            (assign (reg rax) (op logior) (const ,number-tag) (const 1))
            (stack-push (reg rax))
            (call cons)
-           (stack-push (reg rax))
-           (call car))))
-       (rax (get-machine-register machine rax)))
+           (assign (reg sp) (op +) (reg sp) (const 2))
+           (stack-push (reg ret))
+           (call car)
+           (stack-pop))))
+       (ret (get-machine-register machine ret)))
   (start-machine machine)
-  (test-eqv (get-register-contents rax) (logior number-tag 1)))
+  (test-eqv (get-register-contents ret) (logior number-tag 1)))
 
 ;;; Test car: invalid pair
 (let* ((machine
         (make-test-machine
          `((assign (reg rax) (op logior) (const ,number-tag) (const 1))
            (stack-push (reg rax))
-           (call car)))))
+           (call car)
+           (stack-pop)))))
   (test-error #t (start-machine machine)))
 
 ;;; Test cdr: valid pair
@@ -237,18 +252,21 @@
            (assign (reg rbx) (op logior) (const ,number-tag) (const 1))
            (stack-push (reg rax))
            (call cons)
-           (stack-push (reg rax))
-           (call cdr))))
-       (rax (get-machine-register machine rax)))
+           (assign (reg sp) (op +) (reg sp) (const 2))
+           (stack-push (reg ret))
+           (call cdr)
+           (stack-pop))))
+       (ret (get-machine-register machine ret)))
   (start-machine machine)
-  (test-eqv (get-register-contents rax) (logior number-tag 2)))
+  (test-eqv (get-register-contents ret) (logior number-tag 2)))
 
 ;;; Test cdr: invalid pair
 (let* ((machine
         (make-test-machine
          `((assign (reg rax) (op logior) (const ,number-tag) (const 1))
            (stack-push (reg rax))
-           (call cdr)))))
+           (call cdr)
+           (stack-pop)))))
   (test-error #t (start-machine machine)))
 
 (test-end "asm-interpreter-test")
