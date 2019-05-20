cf147136c9ec8933a548bf8bffe4e65f1102fe92
Alex Bates <ard.bates@gmail.com>
2019-05-20 22:59:46 +0100

    Implement (currently non-functional) garbage collector

--- asm-interpreter.scm
+++ asm-interpreter.scm
@@ -10,6 +10,7 @@
 (define value-mask #x0fffffff)
 (define pair-tag #x10000000)
 (define number-tag #x20000000)
+(define broken-heart #x30000000)
 
 ;;; Register aliases
 (define ret 0)                          ; Used for return value
@@ -23,6 +24,8 @@
 (define the-cars-pointer 0)
 (define the-cdrs-pointer 1)
 (define free-pair-pointer 2)            ; Next unassigned index into the pairs arrays
+(define new-cars-pointer 3)
+(define new-cdrs-pointer 4)
 (define the-cars-offset 8)
 
 ;;; Exit codes
@@ -30,6 +33,7 @@
 ;;; 2 - attempting to take CDR of a non-pair
 ;;; 3 - attempting to set the CAR of a non-pair
 ;;; 4 - attempting to set the CAR of a non-pair
+;;; 5 - no space for a new pair
 
 (define (init max-num-pairs)
   `((alias ,ret ret)
@@ -47,9 +51,17 @@
 
     ;; Initialise free-pair-pointer
     (assign (reg rax) (const 0))
-    (mem-store (const ,free-pair-pointer) (reg rax))))
+    (mem-store (const ,free-pair-pointer) (reg rax))
 
-(define (memory-management-defs max-num-pairs)
+    ;; Initialise new-cars-pointer
+    (assign (reg rax) (const ,(+ the-cars-offset (* 2 max-num-pairs))))
+    (mem-store (const ,new-cars-pointer) (reg rax))
+
+    ;; Initialise new-cdrs-pointer
+    (assign (reg rax) (const ,(+ the-cars-offset (* 3 max-num-pairs))))
+    (mem-store (const ,new-cdrs-pointer) (reg rax))))
+
+(define (memory-management-defs max-num-pairs memory-size)
   `(
     ;; Args:
     ;; 0 - car of new pair
@@ -60,6 +72,18 @@
     (stack-push (reg rax))
     (stack-push (reg rbx))
     (stack-push (reg rcx))
+    ;; Trigger garbage collection when no pairs are available
+    (mem-load (reg rax) (const ,free-pair-pointer))
+    (test (op >=) (reg rax) (const ,max-num-pairs))
+    (jez (label cons-after-gc))
+    (call gc)
+    ;; Throw error if no space exists after garbage collection
+    (mem-load (reg rax) (const ,free-pair-pointer))
+    (test (op >=) (reg rax) (const ,max-num-pairs))
+    (jez (label cons-after-gc))
+    (error (const 5))
+
+    cons-after-gc
     (mem-load (reg rax) (const ,free-pair-pointer))
     (mem-load (reg rbx) (const ,the-cars-pointer))
     (assign (reg rbx) (op +) (reg rax) (reg rbx))
@@ -182,20 +206,132 @@
        (lambda (i)
          `(stack-push (reg ,i)))
        (range 0 max-num-pairs))
+    (mem-store (const ,free-pair-pointer) (const 0))
+    ;; Relocate all pairs on stack
+    (assign (reg rax) (reg sp))         ; Stack index pointer
 
+    gc-stack-relocate
+    (test (op <) (reg rax) (const ,memory-size))
+    (jez (label gc-after-stack-relocate))
+    (stack-push (reg rax))
+    (call gc-relocate-pair)
+    (stack-pop)
+    (assign (reg rax) (op +) (reg rax) (const 1))
+    (goto (label gc-stack-relocate))
+
+    gc-after-stack-relocate
+    (assign (reg rax) (const 0))        ; Scan pointer
+    (mem-load (reg rbx) (const ,new-cars-pointer))
+    (mem-load (reg rcx) (const ,new-cdrs-pointer))
+
+    gc-scan
+    (mem-load (reg rbx) (const ,free-pair-pointer))
+    (test (op <) (reg rax) (reg rbx))
+    (jez (label gc-after-scan))
+    ;; Relocate CAR of current pair
+    (assign (reg rdx) (op +) (reg rax) (reg rbx))
+    (stack-push (reg rdx))
+    (call gc-relocate-pair)
+    (stack-pop)
+    ;; Relocate CDR of current pair
+    (assign (reg rdx) (op +) (reg rax) (reg rcx))
+    (stack-push (reg rdx))
+    (call gc-relocate-pair)
+    (stack-pop)
+    (assign (reg rax) (op +) (reg rax) (const 1))
+    (goto (label gc-scan))
+
+    gc-after-scan
+    ;; Swap the-cars with new-cars
+    (mem-load (reg rax) (const ,the-cars-pointer))
+    (mem-load (reg rbx) (const ,new-cars-pointer))
+    (mem-store (const ,the-cars-pointer) (reg rbx))
+    (mem-store (const ,new-cars-pointer) (reg rax))
+    ;; Swap the-cdrs with new-cdrs
+    (mem-load (reg rax) (const ,the-cdrs-pointer))
+    (mem-load (reg rbx) (const ,new-cdrs-pointer))
+    (mem-store (const ,the-cdrs-pointer) (reg rbx))
+    (mem-store (const ,new-cdrs-pointer) (reg rax))
     ;; Restore pushed registers
     ,@(map
        (lambda (i)
          `(stack-pop (reg ,i)))
        (reverse (range 0 max-num-pairs)))
-    (ret)))
+    (ret)
+
+    ;; Args:
+    ;; 0 - memory location of object to relocate
+    ;; TODO: not all registers are used by all branches - optimise this.
+    ;; TODO: some pushes and pops of function arguments are unnecessary
+    gc-relocate-pair
+    (stack-push (reg rax))
+    (stack-push (reg rbx))
+    (stack-push (reg rcx))
+    (stack-push (reg rdx))
+    (mem-load (reg rax) (op +) (reg bp) (const 2)) ; Arg 0
+    (mem-load (reg rbx) (reg rax))   ; Candidate object for relocation
+    (stack-push (reg rbx))
+    (call pair?)
+    (stack-pop)
+    (jez (label gc-relocate-pair-end))
+    (stack-push (reg rbx))
+    (call car)
+    (stack-pop)
+    (test (op =) (reg ret) (const ,broken-heart))
+    (jne (label gc-relocate-pair-already-moved))
+    ;; Relocate CAR of old pair to new memory
+    (stack-push (reg rbx))
+    (call car)
+    (stack-pop)
+    (mem-load (reg rcx) (const ,new-cars-pointer))
+    (mem-load (reg rdx) (const ,free-pair-pointer))
+    (assign (reg rcx) (op +) (reg rcx) (reg rdx)) ; Offset into new-cars
+    (mem-store (reg rcx) (reg ret))
+    ;; Relocate CDR of old pair to new memory
+    (stack-push (reg rbx))
+    (call cdr)
+    (stack-pop)
+    (mem-load (reg rcx) (const ,new-cdrs-pointer))
+    (mem-load (reg rdx) (const ,free-pair-pointer))
+    (assign (reg rcx) (op +) (reg rcx) (reg rdx)) ; Offset into new-cdrs
+    (mem-store (reg rcx) (reg ret))
+    ;; Set CAR of old pair to broken heart
+    (stack-push (const ,broken-heart))
+    (stack-push (reg rax))
+    (call set-car!)
+    (assign (reg sp) (op +) (reg sp) (const 2))
+    ;; Set CDR of old pair to FREE-PAIR-POINTER
+    (stack-push (reg rdx))
+    (stack-push (reg rax))
+    (call set-cdr!)
+    (assign (reg sp) (op +) (reg sp) (const 2))
+    ;; Increment FREE-PAIR-POINTER
+    (assign (reg rdx) (op +) (reg rdx) (const 1))
+    (mem-store (const ,free-pair-pointer) (reg rdx))
+    (goto (label gc-relocate-pair-end))
+
+    gc-relocate-pair-already-moved
+    ;; Point location to CDR of already-moved pair
+    (stack-push (reg rbx))
+    (call cdr)
+    (stack-pop)
+    (assign (reg rbx) (op logior) (reg ret) (const ,pair-tag))
+    (mem-store (reg rax) (reg rbx))
+
+    gc-relocate-pair-end
+    (stack-pop (reg rdx))
+    (stack-pop (reg rcx))
+    (stack-pop (reg rbx))
+    (stack-pop (reg rax))
+    (ret)
+    ))
 
 ;;; Utilities
 
-(define (wrap-code max-num-pairs code)
+(define (wrap-code max-num-pairs memory-size code)
   `(,@(init max-num-pairs)
     (goto (label start))
-    ,@(memory-management-defs max-num-pairs)
+    ,@(memory-management-defs max-num-pairs memory-size)
     start
     ,@code))
 
@@ -219,7 +355,7 @@
   (make-machine-load-text
    test-num-registers
    test-memory-size
-   (wrap-code test-max-num-pairs code)))
+   (wrap-code test-max-num-pairs test-memory-size code)))
 
 (test-begin "asm-interpreter-test")
 
@@ -295,7 +431,7 @@
         (make-test-machine
          `((assign (reg rax) (op logior) (const ,number-tag) (const 2))
            (stack-push (reg rax))
-           (assign (reg rbx) (op logior) (const ,number-tag) (const 1))
+           (assign (reg rax) (op logior) (const ,number-tag) (const 1))
            (stack-push (reg rax))
            (call cons)
            (assign (reg sp) (op +) (reg sp) (const 2))
@@ -378,5 +514,20 @@
            (call set-cdr!)
            (assign (reg sp) (op +) (reg sp) (const 2))))))
   (test-error #t (start-machine machine)))
+
+;;; Test gc: single preserved pair
+(let* ((machine
+        (make-test-machine
+         `((assign (reg rax) (op logior) (const ,number-tag) (const 2))
+           (stack-push (reg rax))
+           (assign (reg rax) (op logior) (const ,number-tag) (const 1))
+           (stack-push (reg rax))
+           (call cons)
+           (stack-pop)
+           (assign (reg rax) (reg ret))
+           (call gc))))
+       (rax (get-machine-register machine rax)))
+  (start-machine machine)
+  (test-eqv (get-register-contents rax) (logior pair-tag 0)))
 
 (test-end "asm-interpreter-test")
