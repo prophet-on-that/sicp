112cbcd22f1b87f6aa5b28da75841215ca33e5c0
Alex Bates <ard.bates@gmail.com>
2020-01-21 13:58:12 +0000

    Remove inadvertent trace-all in test

--- asm-interpreter.scm
+++ asm-interpreter.scm
@@ -4013,7 +4013,6 @@ EVAL for magic value not accessible to the programmer"
             (assign (reg rcx) (const ,(+ read-buffer-offset test-read-buffer-size)))
             ,@(call 'sprint 'rax 'rbx 'rcx))
           #:max-num-pairs max-num-pairs)))
-   (set-machine-trace-all machine 'full)
    (start-machine machine)
    (test-eqv (get-register-contents (get-machine-register machine ret))
      (+ read-buffer-offset 4))))
